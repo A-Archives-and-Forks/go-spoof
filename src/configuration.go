d8dbf928bc3845d28328bf2d2389e02d6c79c5bf
Joseph Boyd <josephboyd113@gmail.com>
2025-02-03 14:16:48 -0700

    adding collection of default IP address

--- src/configuration.go
+++ src/configuration.go
@@ -17,6 +17,9 @@ package main
 
 import (
 	"flag"
+	"net"
+	"log"
+	"os"
 )
 
 type Config struct {
@@ -33,8 +36,15 @@ func config() Config{
 
 	var configuration Config;
 
-	configuration.IP 					    = flag.String("i", "default", "ip : Bind to a particular IP address")
-	configuration.Port 					= flag.String("p", "default", "port : bind to a particular PORT number")
+	//Get default IP
+	addr := getIP()
+	if addr == "1" {
+		log.Println("Error getting default IP - try manually providing the IP")
+		os.Exit(1)
+	}
+
+	configuration.IP 					    = flag.String("i", addr, "ip : Bind to a particular IP address")
+	configuration.Port 					= flag.String("p", "4444", "port : bind to a particular PORT number")
 	configuration.ServiceSignaturePath 	= flag.String("s", "default", "file_path : portspoof service signature regex. file")
 	configuration.ConfigurationFilePath = flag.String("c", "default", "file_path : portspoof configuration file")
 	configuration.LoggingFilePath		= flag.String("l", "default", "file_path : log port scanning alerts to a file")
@@ -42,4 +52,22 @@ func config() Config{
 	configuration.Verbosity 				= flag.String("v", "default", "be verbose")
 	flag.Parse()
 	return configuration
+}
+
+func getIP() string {
+	addr, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Println(err)
+		return "1"
+	}
+
+
+	for _, addr := range addr {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil || ipnet.IP.To16 != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "1"
 }
